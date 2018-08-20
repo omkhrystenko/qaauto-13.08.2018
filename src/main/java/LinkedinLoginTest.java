97353bf294881cb3512b37ac209bf8afc2d33088
omkhrystenko <oleksandrkhrystenko@gmail.com>
2018-08-20 14:40:06 +0300

    Lesson 3

--- src/main/java/LinkedinLoginTest.java
+++ src/main/java/LinkedinLoginTest.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.Test;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
 
 public class LinkedinLoginTest {
 
@@ -31,39 +34,61 @@ public class LinkedinLoginTest {
         driver.get(mainURL);
 
         Assert.assertEquals(driver.getCurrentUrl(), mainURL, "Login page URL doesn't not match");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up", "Login page title is wrong");
 
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
+        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
+        WebElement signInButton = driver.findElement(By.xpath("//input[@id = 'login-submit']"));
 
-        String loginMail = "";
+        Assert.assertTrue(signInButton.isDisplayed(), "sign in button is not displayed on login page");
 
-        String password = "";
+        userEmailField.sendKeys("");
+        userPasswordField.sendKeys("");
+        signInButton.click();
 
-        WebElement loginMailInput = driver.findElement(By.id("login-email"));
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Home page URL is wrong");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn", "Home page Title is wrong");
 
-        WebElement passwordInput = driver.findElement(By.id("login-password"));
-
-        WebElement signInInput = driver.findElement(By.id("login-submit"));
-
-
-
-        loginMailInput.clear();
-        loginMailInput.click();
-        loginMailInput.sendKeys(loginMail);
-
-        passwordInput.clear();
-        passwordInput.click();
-        passwordInput.sendKeys(password);
-
-        signInInput.click();
-
-        WebElement me = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"nav-settings__dropdown-trigger\"]/div/span[1]")));
-        String meWebElement =  me.getText();
-        String controlString = "Me";
-
-        Assert.assertEquals(meWebElement, controlString);
+        WebElement profileNavItem = driver.findElement(By.xpath("//li[@id = 'profile-nav-item']"));
+        Assert.assertTrue(profileNavItem.isDisplayed(), "profileNavItem button is not displayed on Home page");
 
         driver.quit();
+    }
 
+    @Test
+    public void negativeloginTest(){
+        System.setProperty("webdriver.chrome.driver", "D:\\WebDrivers\\chromedriver.exe");
 
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        String mainURL = "https://www.linkedin.com/";
+
+        driver.get(mainURL);
+
+        Assert.assertEquals(driver.getCurrentUrl(), mainURL, "Login page URL doesn't not match");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up", "Login page title is wrong");
+
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
+        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
+        WebElement signInButton = driver.findElement(By.xpath("//input[@id = 'login-submit']"));
+
+        Assert.assertTrue(signInButton.isDisplayed(), "sign in button is not displayed on login page");
+
+        userEmailField.sendKeys("a@b.c");
+        userPasswordField.sendKeys("wrong");
+        signInButton.click();
+
+        try {
+            sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement alertMessage = driver.findElement(By.xpath("//div[@role = 'alert']"));
+        Assert.assertEquals(alertMessage.getText(), "There were one or more errors in your submission. Please correct the marked fields below.", "Alert message text is wrong");
+
+        driver.quit();
     }
 
 
