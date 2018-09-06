04c2b84fe8e675c1f4dd71c929a197640ee65e18
omkhrystenko <oleksandrkhrystenko@gmail.com>
2018-09-06 12:07:56 +0300

    Lesson 6

--- src/test/java/LinkedinSearchTest.java
+++ src/test/java/LinkedinSearchTest.java
@@ -7,13 +7,13 @@ public class LinkedinSearchTest extends LinkedinBaseTest {
     @DataProvider
     public Object[][] search_HomePage() {
         return new Object[][]{
-                {"autotestqa2018@gmail.com", "trust2018", "hr"}
+                {"autotestqa2018@gmail.com", "trust2018", "hr", 10}
         };
     }
 
 
     @Test(dataProvider = "search_HomePage")
-    public void searchTestHomePage(String userEmail, String userPassword, String requestData){
+    public void searchTestHomePage(String userEmail, String userPassword, String requestData, int resCount){
         LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(driver, driverWait);
         Assert.assertTrue(linkedinLoginPage.isPageLoaded(), "Login page is not loaded");
 
@@ -26,8 +26,8 @@ public class LinkedinSearchTest extends LinkedinBaseTest {
         Assert.assertTrue(linkedinHomePage.isSearchContainerDisplayed(), "SearchForm is not displayed");
         Assert.assertEquals(linkedinHomePage.allFiltersButtonGetName(), "All Filters", "Controll button AllFilters does not match");
 
-        Assert.assertEquals(linkedinHomePage.quantityOfSearchFormAnswers(), 10, "Quantity of answers on search form is wrong");
-        Assert.assertEquals(linkedinHomePage.quantityOfMatchResults(requestData), 10, "Not all the results coincided");
+        Assert.assertEquals(linkedinHomePage.quantityOfSearchFormAnswers(), resCount, "Quantity of answers on search form is wrong");
+        Assert.assertEquals(linkedinHomePage.quantityOfMatchResults(requestData), resCount, "Not all the results coincided");
 
     }
 
